e0da4242f9cbbedfadb4baf1c3f75c3ce9338264
Abdul Rauf <rauf11.abdul@gmail.com>
2024-10-14 15:14:18 +0500

    added timeout

--- appConfig.js
+++ appConfig.js
@@ -2,6 +2,9 @@
 
 const APPLICATION_SLUG = 'career-quiz-application'; // Define the slug for your application
 
+const INACTIVITY_TIMER = 60000;
+
 module.exports = {
   APPLICATION_SLUG,
+  INACTIVITY_TIMER,
 };
